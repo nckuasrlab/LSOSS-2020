c5778b7b9eb44ddba67006375fb8ef0f37f86d9b
BClown1229 <brian891229@gmail.com>
2020-09-30 12:31:13 +0800

    finish lab_4

---
+++ f74086103/lab_4_vim/hello.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+#include <string.h>
+void main(){
+	const char s[] = {'H','e','l','l','o',' ','F','7','4','0','8','6','1','0','3'};
+	printf("%s\n", s);
+	int a = 3 / 1;
+	printf("%d\n", a);
+	return ;
+}
