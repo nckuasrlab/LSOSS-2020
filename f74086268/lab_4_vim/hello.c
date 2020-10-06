fb864a116348765e2d2d6986a34b2b9521d6d415
wish99benjamin <wish99benjamin@gmail.com>
2020-10-06 22:13:12 +0800

    f74086268

---
+++ f74086268/lab_4_vim/hello.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main() {
+	const char s[]= "Hello f74086268";
+	for(int temp = 0;temp<15;temp++)
+	{
+		printf("%c", s[temp]);
+	}
+	printf("\n");
+	int a = 3 / 1;
+	printf("%d\n", a);
+	return a;
+}
